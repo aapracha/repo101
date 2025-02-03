6eb551d65cbc4da7c7cf9e6bd057df33458d72cf
aparacha <ayubparac@gmail.com>
2025-02-02 23:22:51 -0500

    first attempt

---
+++ String_methods.py
@@ -0,0 +1,9 @@
+# print("Hello world")
+name="John"
+print(name[3])
+print(name[-1])
+#reverse indexing 
+print(name[-1:-5:-1])
+print(name[-1::-1])
+x="this is my initial first code"
+print(x[-1::-1])
---
+++ first.py
@@ -0,0 +1,38 @@
+print("Hello world")
+#print("statement 2")
+#a="Aqeel"
+#b="Ayub"
+#print("Mynameis", a)
+#print(12,45,56,89,sep='%')
+#print(5+5)
+#print(5*5)
+#print(5-5)
+#print(a,b, sep='_')
+#help('keywords')
+
+#a=12
+#b=23
+#c=a+b
+# print(c)
+
+# a,b,c = 12,23,34
+# print(a+b+c)
+# Fname,Mname,Lname='Aqeel','Ayub','Paracha'
+# print(Fname,Mname,Lname)
+# a=b=c=12
+# print(a,b,c)
+# print("Variable a type is", type(a))
+# print(type(Mname))
+# import keyword
+
+
+# keyword.kwlist
+# x=keyword.kwlist
+# print(x)
+# print(len(x))
+
+x="python"
+print(x)
+print(len(x))
+print(x[1:4:2])
+print(x[1: ])
